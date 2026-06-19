e62309ecd817a9775042d3af5e5b4898b25a48dc
Kerem Şener <blogkeremsener@gmail.com>
2026-06-19 16:14:25 +0300

    Finished: ft_alembic_2.py

---
+++ ft_alembic_2.py
@@ -0,0 +1,7 @@
+import alchemy.elements
+
+if __name__ == "__main__":
+    elements = alchemy.elements
+    print("=== Alembic 2 ===")
+    print("Accessing alchemy/elements.py using 'import ...' structure")
+    print("Testing create_earth:", elements.create_earth())
