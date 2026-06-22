bd28c9d31bed5c19356657fad38795f62a07d53c
ksener <blogkeremsener@gmail.com>
2026-06-22 11:07:44 +0300

    Added ft_transmutation_1.py

---
+++ ft_transmutation_1.py
@@ -0,0 +1,7 @@
+import alchemy.transmutation
+
+if __name__ == "__main__":
+    print("=== Transmutation 1 ===")
+    path =  alchemy.transmutation
+    print("Import transmutation module directly")
+    print("Testing lead to gold:", path.lead_to_gold())
